868954f1ca17c50a479df6a203553bea72409f7d
Gus Kemenyfy <gustavo.kemenyfy@gmail.com>
2019-01-21 08:34:42 -0500

    Add project files.

---
+++ AngularJSTutorial/app/controllers/MainController.js
@@ -0,0 +1,17 @@
+﻿app.controller('MainController', function ($scope, $http) {
+
+    var onUserComplete = function (response) {
+        $scope.user = response.data;
+
+    };
+
+    var onError = function (reason) {
+        $scope.error = "Could not fetch the user";
+    };
+
+    $http.get("https://api.github.com/users/ambethia")
+        .then(onUserComplete, onError);
+
+    $scope.Message = "Hello, Angular";
+
+});
